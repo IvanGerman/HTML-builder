d318b3e9a1b6ba525c053d1d512266d05c95084a
IvanGerman <ivgvi@mail.ru>
2021-11-06 22:39:20 +0700

    feat: task 04-copy-directory ready

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs/promises'); //use promise-based API
 
+
+async function copyDir() {
+
+  try {
+
+    await fsPromises.access(pathToCopyDir, fs.constants.R_OK | fs.constants.W_OK);// check if directory exists
+
+    const previousFiles = await fsPromises.readdir(pathToCopyDir);
+
+    previousFiles.forEach(async (element) => {
+
+      const filePath = path.join(pathToCopyDir, element);
+      fsPromises.unlink(filePath); //delete file
+
+    });
+
+  } catch {
+
+    await fsPromises.mkdir(pathToCopyDir, { recursive: true }); //create directory if it's not existing
+                                          //returns first directory path created if recursive is true
+  };
+
+
+  const allFiles = await fsPromises.readdir(pathToFilesDir);//get all files from /files directory
+
+  allFiles.forEach( async (element) => { 
+
+    try {
+
+      const sourceDirFile = path.join(__dirname, 'files', element);
+      const destDirFile = path.join(__dirname, 'files-copy', element);
+      await fsPromises.copyFile(sourceDirFile, destDirFile);
+
+    } catch {
+
+      console.log('The file could not be copied');
+
+    };
+
+  });
+};
+
+const pathToCopyDir = path.join(__dirname, 'files-copy');
+const pathToFilesDir = path.join(__dirname, 'files');
+
+copyDir();
